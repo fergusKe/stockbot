e880764f40259861aece63dfd2f2622b0152c7f5
fergusKe <fergus.jingtai@gmail.com>
2018-09-16 12:08:36 +0800

    update db

--- mongodb.py
+++ mongodb.py
@@ -10,12 +10,12 @@ from pymongo import MongoClient
 import urllib.parse
 import datetime
 
-username = urllib.parse.quote_plus('你的帳號') 
-password = urllib.parse.quote_plus('你的密碼')
-host = '你的主機位置' #主機位置
-port = 'port號碼' #port號碼
-dbname='你的資料庫名稱'
-collection='你的資料庫名稱'
+username = urllib.parse.quote_plus('fergus') 
+password = urllib.parse.quote_plus('31iwillinto')
+host = 'ds155644.mlab.com' #主機位置
+port = '55644' #port號碼
+dbname='stockbot'
+collection='stockbot'
 
 ###############################################################################
 #                           LineBot股票機器人mongoDB#                            #
